23825196744a625bd19d7642390a08f970a4049b
Smit-whitespace <143988078+Smit-whitespace@users.noreply.github.com>
2026-04-15 09:38:12 +0530

    Update CORS middleware to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -74,7 +74,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[ *
+        allow_origins=[ "*"
             # "http://localhost:5173",
             # "http://127.0.0.1:5173"
         ],
